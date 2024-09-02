02c92afe92bf120a0f0128c220022ba5964bd78c
Angel <angelsanchez.as05@gmail.com>
2024-09-02 18:58:01 -0500

    first commit

--- Ocean-Game/Assets/Scripts/PlayerController.cs
+++ Ocean-Game/Assets/Scripts/PlayerController.cs
@@ -6,19 +6,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
    
-    
+
     public float speed = 30.0f;
 
     private Rigidbody playerRb;
-    private float movementX; 
-    private float movementY;
+    public float size = 10.0f;
 
-    private float size = 10;
-
+    
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        
 
     }
 
@@ -29,18 +28,35 @@ public class PlayerController : MonoBehaviour
         float movementY = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        playerRb.AddForce(movement * speed);
-    }
+        playerRb.AddForce(movement * speed, ForceMode.Acceleration);
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.CompareTag("Prop") && collision.transform.localScale.magnitude <= size)
-        {
-            Debug.Log("OnCollisoinEnter");
-
-            collision.transform.parent = transform;
-            size += collision.transform.localScale.magnitude;
-        }
-    }
     
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+    if(other.gameObject.CompareTag("Prop") && other.transform.localScale.magnitude <= size)
+        {
+        Debug.Log("OnTriggerEnter");
+
+        // Turning off the colliders of the props
+        Collider propCollider = other.gameObject.GetComponent<Collider>();
+        if(propCollider != null)
+        {
+            propCollider.enabled = false;
+        }
+
+        // Make the objects child of the ball/player 
+        other.transform.parent = transform;
+
+        // Changing the size to reflect the number of stuff collected
+        
+        size += other.transform.localScale.magnitude;
+        }
+
+
+    }
+
 }
+    
+
