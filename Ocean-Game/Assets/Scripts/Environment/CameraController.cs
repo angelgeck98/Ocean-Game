406009317c7cb476484a88eec5d750b7843db1ba
Pablo Rojo <rojonano42@gmail.com>
2024-09-05 19:14:39 -0600

    added cinemachine camera

--- Ocean-Game/Assets/Scripts/Environment/CameraController.cs
+++ Ocean-Game/Assets/Scripts/Environment/CameraController.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    public GameObject player; 
-    private Vector3 offset; 
+    //public GameObject player; 
+    //private Vector3 offset; 
 
     //Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        //offset = transform.position - player.transform.position;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
     }
 
     //Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        //transform.position = player.transform.position + offset;
+        Debug.Log("Cursor Lock State: " + Cursor.lockState);
+        Debug.Log("Cursor Visible: " + Cursor.visible);
     }
 }
