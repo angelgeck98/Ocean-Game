5427e6214e29a4911beb201c9f9515330a905f5d
Angel <angelsanchez.as05@gmail.com>
2024-09-12 11:32:34 -0500

    New UI

--- Ocean-Game/Assets/Scripts/UI/UIPauseMenu.cs
+++ Ocean-Game/Assets/Scripts/UI/UIPauseMenu.cs
@@ -6,50 +6,56 @@ using UnityEngine.UI;
 public class UIPauseMenu : MonoBehaviour
 
 {
-    [SerializeField] Button _BackToMainMenu;
-    [SerializeField] GameObject _pauseMenu; 
 
-    private bool _isPaused = false; //track if paused
+    public static bool GameIsPaused = false; //Just checking if game is paused or not
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        if(_BackToMainMenu == null)
-        {
-            Debug.LogError("ibuttin is not assigned"); 
-            return;
-        }
-
-         _BackToMainMenu.onClick.AddListener(BackToMainMenu);
-
-         _pauseMenu.SetActive(false);
-    }
+    public GameObject pausedMenuUI;
 
     void Update()
     {
-         if(Input.GetKeyDown(KeyCode.Escape))
-         {
-            TogglePauseMenu();
-         }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+
+
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
+        }
     }
 
-    private void BackToMainMenu()
+
+    public void Resume()
     {
-        if(ScenesManager.Instance != null)
-        {
-            ScenesManager.Instance.LoadScene("MainMenu");
-        }
-        else 
-        {
-            Debug.LogError("Scene instance is null");
-        }
-       
+        pausedMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false; 
     }
 
-    private void TogglePauseMenu()
+    void Pause()
     {
-        _isPaused = !_isPaused; //this is the paused state
-        _pauseMenu.SetActive(_isPaused); // show or hide menu
-        Time.timeScale = _isPaused ? 0 : 1; //pause or unpause
+        pausedMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        ScenesManager.Instance.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitting!!");
+        Application.Quit();
+    }
+
 }
+
--- Ocean-Game/Assets/Scripts/UI/UIScriptMENU.cs
+++ Ocean-Game/Assets/Scripts/UI/UIScriptMENU.cs
@@ -5,25 +5,19 @@ using UnityEngine.UI;
 
 public class UIScript : MonoBehaviour
 {
-    [SerializeField] Button _newGame;
+    
    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        _newGame.onClick.AddListener(StartNewGame);
-        
-    }
-
- 
-
     // Update is called once per frame
-    private void StartNewGame()
+    public void StartNewGame()
     {
         ScenesManager.Instance.LoadScene("GameScene");
     }
 
-    
+    public void QuitGame()
+    {
+        Debug.Log("Quitting!!");
+        Application.Quit();
+    }
 
 
 }
